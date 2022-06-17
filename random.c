179b09d228148cf086c3b10d6b5acb6e6638fa8e
PedroWKS <107721547+PedroWKS@users.noreply.github.com>
2022-06-17 17:39:14 -0300

    Alteração Float para int
    Ajuste realizado para converter os números aleatórios de tipo float %f.6 para tipo int

--- random.c
+++ random.c
@@ -1,12 +1,16 @@
 
+
 #include <stdio.h>
 #include "mtwister.h"
 
 int main() {
   MTRand r = seedRand(1234567);
   int i;
+
   for(i=0; i<10; i++) {
-    printf("%f\n", genRand(&r));
+    printf("%d\n",(int)( genRand(&r)*1000000));
   }
+
   return 0;
 }
+
